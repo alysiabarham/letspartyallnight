5b6ecbb5562a4018b5e6d0c8601672479a7c2b84
Alysia <alysiabarham@gmail.com>
2025-09-18 09:16:59 -0700

    Rebuild index to fix phantom U/M flags

--- frontend/src/theme.ts
+++
@@ -1,8 +0,0 @@
-// src/theme.ts
-import { extendTheme } from "@chakra-ui/react";
-
-const theme = extendTheme({
-  // Minimal theme, as styles are applied directly in components
-});
-
-export default theme;
