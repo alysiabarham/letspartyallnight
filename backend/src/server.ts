3dd3ae811331022bb52e3ba7be8f4ca650d4ece1
Alysia <alysiabarham@gmail.com>
2025-09-25 16:49:54 -0700

    Push frontend and backend for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -205,7 +205,7 @@ if (require.main === module) {
   });
 }
 
-// --- Rate Limiting --- hi Salt
+// --- Rate Limiting ---
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
